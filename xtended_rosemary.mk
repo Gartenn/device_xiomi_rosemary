ad04bd8ed3b6e1ae45cfaae4e4369cdeb0c46e7c
Garten <brilliangarten@gmail.com>
2025-04-07 00:09:14 +0700

    Update and rename sigma_rosemary.mk to xtended_rosemary.mk

--- xtended_rosemary.mk
+++ xtended_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/sigma/config/common.mk)
+$(call inherit-product, vendor/xtended/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := sigma_rosemary
+PRODUCT_NAME := xtended_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
