e720650b604a07ce7a4cdc9bdf77ba43c8a64c87
Garten <brilliangarten@gmail.com>
2025-04-07 20:12:41 +0700

    Update and rename xtended_rosemary.mk to ethereal_rosemary.mk

--- ethereal_rosemary.mk
+++ ethereal_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/xtended/config/common.mk)
+$(call inherit-product, vendor/ethereal/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME := xtended_rosemary
+PRODUCT_NAME   := ethereal_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -27,7 +27,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWXM:user/release-keys
 
-BUILD_MAINTAINER := Garten
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_USES_BLUR := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
