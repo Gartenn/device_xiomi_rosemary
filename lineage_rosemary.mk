624e8bc1e64ad41470187550d80360ff55a43afc
Garten <brilliangarten@gmail.com>
2025-04-08 15:43:12 +0700

    Update and rename legion_rosemary.mk to lineage_rosemary.mk

--- lineage_rosemary.mk
+++ lineage_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/legion/config/common.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME   := legion_rosemary
+PRODUCT_NAME   := lineage_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
