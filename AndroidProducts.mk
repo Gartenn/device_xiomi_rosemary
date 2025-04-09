c5081210a9c4f9bd82b7ba2cb0203d3c7ac536a5
Garten <brilliangarten@gmail.com>
2025-04-09 17:29:12 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/cherish_rosemary.mk
+    $(LOCAL_DIR)/ethereal_rosemary.mk
  
  COMMON_LUNCH_CHOICES := \
-     cherish_rosemary-user \
-     cherish_rosemary-userdebug \
-     cherish_rosemary-eng
+     ethereal_rosemary-user \
+     ethereal_rosemary-userdebug \
+     ethereal_rosemary-eng
