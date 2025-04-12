c281ce7fc038c6ef7bf9f966d86e2b51bc4c1f39
Garten <brilliangarten@gmail.com>
2025-04-12 20:45:20 +0700

    Update and rename colt_rosemary.mk to ethereal_rosemary.mk

--- ethereal_rosemary.mk
+++ ethereal_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+$(call inherit-product, vendor/ethereal/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME   := colt_rosemary
+PRODUCT_NAME   := ethereal_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -30,7 +30,6 @@ BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWX
 SYSTEMUI_OPTIMIZE_JAVA := true
 USE_MOTO_CLOCK := true
 SYSTEM_OPTIMIZE_JAVA := true
-TARGET_SUPPORTS_BLUR := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-COLT_BUILD_MAINTAINER := DrgnPrjkt
+ETHEREAL_MAINTAINER := DrgnPrjkt
