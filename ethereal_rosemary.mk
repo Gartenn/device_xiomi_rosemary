b628db37e4a4e0003b75b25907f5f28b2a716e2d
Garten <brilliangarten@gmail.com>
2025-04-17 15:39:03 +0700

    Update and rename afterlife_rosemary.mk to ethereal_rosemary.mk

--- ethereal_rosemary.mk
+++ ethereal_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+$(call inherit-product, vendor/ethereal/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME   := afterlife_rosemary
+PRODUCT_NAME   := ethereal_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -28,15 +28,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWXM:user/release-keys
 
 SYSTEMUI_OPTIMIZE_JAVA := true
-USE_MOTO_CLOCK := true
 SYSTEM_OPTIMIZE_JAVA := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
-AFTERLIFE_MAINTAINER := DrgnPrjkt
+ETHEREAL_MAINTAINER := DrgnPrjkt
 TARGET_SUPPORTS_BLUR := true
 TARGET_SUPPORTS_TOUCHGESTURES := true
-
-# CoreGapps
-AFTERLIFE_GAPPS := true
-AFTERLIFE_CORE := true
-
