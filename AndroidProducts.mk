0f6fec47bbfd8d345d4b5a45bc0f77b818f31626
Garten <brilliangarten@gmail.com>
2025-04-17 15:37:27 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/afterlife_rosemary.mk
+    $(LOCAL_DIR)/ethereal_rosemary.mk
  
  COMMON_LUNCH_CHOICES := \
-     afterlife_rosemary-user \
-     afterlife_rosemary-userdebug \
-     afterlife_rosemary-eng
+     ethereal_rosemary-user \
+     ethereal_rosemary-userdebug \
+     ethereal_rosemary-eng
