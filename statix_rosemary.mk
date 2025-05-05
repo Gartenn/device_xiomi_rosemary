c5f3d38dba8c911e481c59affa85ddf521909a5b
Garten <brilliangarten@gmail.com>
2025-05-05 22:05:01 +0700

    Update statix_rosemary.mk

--- statix_rosemary.mk
+++ statix_rosemary.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/statix/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
