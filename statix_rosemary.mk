1527a185e5bb9886dae042f50cb7c49af5aafaad
Garten <brilliangarten@gmail.com>
2025-05-05 21:51:39 +0700

    Update and rename spark_rosemary.mk to statix_rosemary.mk

--- statix_rosemary.mk
+++ statix_rosemary.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/statix/config/common_full_phone.mk)
 
 # Inherit from rosemary device
 $(call inherit-product, device/xiaomi/rosemary/device.mk)
 
 PRODUCT_DEVICE := rosemary
-PRODUCT_NAME   := lineage_rosemary
+PRODUCT_NAME   := statix_rosemary
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10S
 PRODUCT_MANUFACTURER := Xiaomi
@@ -29,4 +29,4 @@ BUILD_FINGERPRINT := Redmi/rosemary/rosemary:12/SP1A.210812.016/V13.0.5.0.SFFTWX
 
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_BLUR := true
-HAVOC_MAINTAINER := DrgnPrjkt
+TARGET_DOES_NOT_USE_GAPPS := true
